2bb978fb10ac59754e32ef401de3559494ce52fb
kimjingeun <jindream6128@naver.com>
2023-05-31 02:56:31 +0900

    섹션 2 서블릿 230531 #1

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// /hello로오면 전부 받는다
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.basic.request;
+
+import org.apache.tomcat.util.net.jsse.JSSEUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+            request.getParameterNames().asIterator()
+                            .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String userName = request.getParameter("username");
+        System.out.println("request.getParameter('username') = "+userName);
+        System.out.println();
+
+        String age = request.getParameter("age");
+        System.out.println("request.getParameter('age') = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
+        String[] usernames = request.getParameterValues("username");
+        for(String name : usernames){
+            System.out.println("usernames: "+ name);
+        }
+        System.out.println();
+
+        //여기서 getParameterValues말고는 전부 앞의 파라미터가 찍힌다.
+       }
+}
